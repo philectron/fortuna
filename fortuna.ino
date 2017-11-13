a163cbb17a16b7772d55835c7ccd6c8ca37101fc
Phi Luu <philectron@users.noreply.github.com>
2017-11-13 01:27:24 -0800

    Beautify code. Disable alignments.

--- fortuna.ino
+++ fortuna.ino
@@ -14,26 +14,26 @@
  */
 
 // Required hardware I/O connections
-const int LEFT_PHOTO  = A0;    // left photocell connected to A0
-const int MIC_INPUT   = A3;    // microphone connected to A3
+const int LEFT_PHOTO = A0;     // left photocell connected to A0
+const int MIC_INPUT = A3;      // microphone connected to A3
 const int RIGHT_PHOTO = A5;    // right photocell connected to A5
-const int BUZZER      = 2;     // buzzer connected to 2
-const int LEFT_MOTOR  = 5;     // left motor connected to ~5
+const int BUZZER = 2;          // buzzer connected to 2
+const int LEFT_MOTOR = 5;      // left motor connected to ~5
 const int RIGHT_MOTOR = 10;    // right motor connected to ~10
 
 // Global constants
-const int MOTOR_POWER   = 255; // 50% motor power
+const int MOTOR_POWER = 255;   // 50% motor power
 const int MIC_THRESHOLD = 550; // mic threshold, varies 0-1023
 
 void setup() {
-    pinMode(LEFT_MOTOR,  OUTPUT);
+    pinMode(LEFT_MOTOR, OUTPUT);
     pinMode(RIGHT_MOTOR, OUTPUT);
-    pinMode(BUZZER,      OUTPUT);
+    pinMode(BUZZER, OUTPUT);
 }
 
 void loop() {
     // turn off both motors
-    analogWrite(LEFT_MOTOR,  0);
+    analogWrite(LEFT_MOTOR, 0);
     analogWrite(RIGHT_MOTOR, 0);
 
     // beep a bunch of times
@@ -61,7 +61,7 @@ void loop() {
         // make the "siren" noise by alternating 1200 Hz and 800 Hz tones
         for (int i = 0; i < 5; i++) {
             Beep(BUZZER, 1200, 100);
-            Beep(BUZZER, 800,  100);
+            Beep(BUZZER, 800, 100);
         }
 
         // shake motors back and forth rapidly
@@ -69,7 +69,7 @@ void loop() {
             analogWrite(RIGHT_MOTOR, MOTOR_POWER);
             delay(200);
             analogWrite(RIGHT_MOTOR, 0);
-            analogWrite(LEFT_MOTOR,  MOTOR_POWER);
+            analogWrite(LEFT_MOTOR, MOTOR_POWER);
             delay(200);
             analogWrite(LEFT_MOTOR, 0);
         }
@@ -109,7 +109,7 @@ void loop() {
  */
 void Beep(int pin, int freq, long ms) {
     long semi_period = (long)(1000000 / (freq * 2));
-    long num_loops   = (long)((ms * 1000) / (semi_period * 2));
+    long num_loops = (long)((ms * 1000) / (semi_period * 2));
 
     for (int k = 0; k < num_loops; k++) {
         digitalWrite(pin, HIGH);        // set buzzer pin high
